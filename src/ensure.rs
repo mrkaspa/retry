2aa13ddaf084bc758378ba3b273cc9bbefc46140
Michel Perez <michel.ingesoft@gmail.com>
2019-08-18 14:43:20 -0500

    wip

--- src/ensure.rs
+++ src/ensure.rs
@@ -1,27 +1,60 @@
-use crate::utils::GeneralError;
-use actix_web::{self, web, HttpRequest};
-use serde::Deserialize;
+use crate::utils::{GeneralError, RetryError};
+use actix_http::http::{header, Method};
+use actix_http::RequestHead;
+use actix_web::client::Client;
+use actix_web::{self, web, HttpRequest, HttpResponse};
+use futures::future::{lazy, Future};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct Payload {
+pub struct RetryPayload {
     reference: String,
     retries: u32,
     method: String,
+    headers: Vec<(String, String)>,
     request_url: String,
     payload: String,
 }
 
-pub struct Response {}
-
-pub fn execute(
-    (req, json): (HttpRequest, web::Json<Payload>),
-) -> actix_web::Result<String, GeneralError> {
-    let payload = json.into_inner();
-    ensure(payload);
-    Err(GeneralError {
-        status: 402,
-        message: String::from("missing name param"),
-    })
+#[derive(Serialize)]
+pub struct RetryResponse {
+    message: String,
 }
 
-fn ensure(payload: Payload) {}
+pub fn execute(json: web::Json<RetryPayload>) -> actix_web::Result<HttpResponse, GeneralError> {
+    let payload = json.into_inner();
+    match ensure(payload) {
+        Ok(()) => Ok(HttpResponse::Ok().json(RetryResponse {
+            message: String::from("all ok"),
+        })),
+        Err(_) => Err(GeneralError {
+            status: 402,
+            message: String::from("missing name param"),
+        }),
+    }
+}
+
+fn ensure(payload: RetryPayload) -> Result<(), RetryError> {
+    let mut sys = actix::System::new("request");
+    sys.block_on(lazy(|| {
+        let client = Client::default();
+        let mut req_head = RequestHead::default();
+        for tuple in payload.headers.iter() {
+            let (header_, value) = tuple.clone();
+            req_head.headers.append(
+                header::HeaderName::from_bytes(&header_.into_bytes()).unwrap(),
+                header::HeaderValue::from_bytes(&value.into_bytes()).unwrap(),
+            );
+        }
+        req_head.method = Method::from_bytes(&payload.method.into_bytes()).unwrap();
+        client
+            .request_from(payload.request_url, &req_head)
+            .send_body(payload.payload)
+            .map_err(|_| RetryError { retry_no: 0 })
+            .and_then(|response| {
+                // <- server http response
+                println!("Response: {:?}", response);
+                Ok(())
+            })
+    }))
+}
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use actix_web::{self, error, http::StatusCode, HttpResponse};
 use failure::Fail;
 use serde::Serialize;
+use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, Serialize, Fail)]
 #[fail(display = "my error")]
@@ -14,3 +16,16 @@ impl error::ResponseError for GeneralError {
         HttpResponse::build(StatusCode::from_u16(self.status).unwrap()).json2(self)
     }
 }
+
+#[derive(Debug)]
+pub struct RetryError {
+    pub retry_no: u32,
+}
+
+impl fmt::Display for RetryError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "RetryError is here!")
+    }
+}
+
+impl Error for RetryError {}
